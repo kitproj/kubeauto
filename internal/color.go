aabc40a30619dfed3dbd46b6b7b186776fa3428e
Alex Collins <alex_collins@intuit.com>
2025-01-12 09:59:07 -0800

    fix: do not use bright or dark colors as these do not work on white or black backgrounds

--- internal/color.go
+++ internal/color.go
@@ -6,6 +6,17 @@ import (
 	"io"
 )
 
+// https://github.com/gawin/bash-colors-256
+// not too dark or light
+var colors = []int{
+	34, 35, 36, 37, 38, 39,
+	70, 71, 72, 73, 74, 75,
+	106, 107, 108, 109, 110, 111,
+	142, 143, 144, 145, 146, 147,
+	178, 179, 180, 181, 182, 183,
+	214, 215, 216, 217, 218, 219,
+}
+
 // colorize the text based on the name
 func color(name, text string) string {
 	return fmt.Sprintf("\x1b[38;5;%dm%s\x1b[0m", code(name), text)
@@ -16,8 +27,7 @@ func code(name string) int {
 	for _, r := range name {
 		code += int(r)
 	}
-	// 16 to 231
-	return 16 + code%216
+	return colors[code%len(colors)]
 }
 
 func colorWriter(name, prefix string) io.Writer {
