086607c3cba03bc5fa10cdf2d5296b0c0d9ec26b
Alex Collins <alex_collins@intuit.com>
2025-01-20 09:26:12 -0800

    fix: change default port offset to zero

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	flag.StringVar(&namespace, "n", "", "namespace to filter resources, defaults to the current namespace ")
 	flag.StringVar(&labels, "l", "", "comma separated list of labels to filter resources, e.g. app=nginx, defaults to all resources")
 	flag.StringVar(&group, "g", "", "the group to watch, defaults to core resources")
-	flag.IntVar(&hostPortOffset, "p", 30000, "the offset to add to the host port, e.g. if the container listens on 8080 and the host port is 30000, the offset is 38080, defaults to 30000")
+	flag.IntVar(&hostPortOffset, "p", 0, "the offset to add to the host port, e.g. if the container listens on 8080 and the host port is 30000, the offset is 38080, defaults to 0")
 	flag.BoolVar(&help, "h", false, "print help")
 	flag.Parse()
 
