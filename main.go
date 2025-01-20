6a806820cc445df9d4913190364baf25804f60a2
Alex Collins <alex_collins@intuit.com>
2025-01-20 10:13:01 -0800

    feat: print version with `-v`

--- main.go
+++ main.go
@@ -3,24 +3,34 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/kitproj/kubeauto/internal"
 	"os"
 	"os/signal"
+	"runtime/debug"
 )
 
 func main() {
 	var namespace, labels, group string
 	var hostPortOffset int
 	var help bool
+	var version bool
 	flag.StringVar(&namespace, "n", "", "namespace to filter resources, defaults to the current namespace ")
 	flag.StringVar(&labels, "l", "", "comma separated list of labels to filter resources, e.g. app=nginx, defaults to all resources")
 	flag.StringVar(&group, "g", "", "the group to watch, defaults to core resources")
 	flag.IntVar(&hostPortOffset, "p", 0, "the offset to add to the host port, e.g. if the container listens on 8080 and the host port is 30000, the offset is 38080, defaults to 0")
 	flag.BoolVar(&help, "h", false, "print help")
+	flag.BoolVar(&version, "v", false, "print version")
 	flag.Parse()
 
 	if help {
 		flag.Usage()
+		return
+	}
+
+	if version {
+		info, _ := debug.ReadBuildInfo()
+		fmt.Println(info.Main.Version)
 		return
 	}
 
